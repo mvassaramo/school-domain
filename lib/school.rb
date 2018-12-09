4fbc3f85f26e06ff1f69605ac7ed6de03a7dd6fd
github username <maduri.vassaramo@gmail.com>
2018-12-09 13:48:39 +0000

    done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+
+    def add_student(student_name, grade)
+        roster[grade]  ||= []
+        roster[grade] << student_name 
+    end
+
+    def grade(student_grade)
+        roster[student_grade]
+    end 
+
+    def sorted 
+        sorted = {}
+        roster.each do |grade, students|
+            sorted[grade] = students.sorted
+        end
+    end
+
+
+end 
